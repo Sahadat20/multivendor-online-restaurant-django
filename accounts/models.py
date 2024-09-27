f3d13b920632d017d1c2f741aedc9d9a5a3d390b
sahadat20 <shahadathossainroni941@gmail.com>
2024-09-27 11:56:55 +0600

    Vendor and Customer Dashboard make dybamic

--- accounts/models.py
+++ accounts/models.py
@@ -99,6 +99,8 @@ class UserProfile(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     modifed_at = models.DateTimeField(auto_now=True)
 
+    def full_address(self):
+        return (f'{self.address_line1}, {self.address_line2}')
     def __str__(self):
         return self.user.email
 
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path 
+from django.urls import path, include
 from . import views
 urlpatterns =[
+    path('', views.myAccount),
     path('register-user/', views.registerUser, name='registerUser' ),
     path('register-vendor/', views.registerVendor, name='registerVendor' ),
     path('login/', views.login, name='login' ),
@@ -13,5 +14,5 @@ urlpatterns =[
     path('forgot_password/', views.forgot_password, name='forgot_password'),
     path('reset_password_validate/<uidb64>/<token>/', views.reset_password_validate, name='reset_password_validate' ),
     path('reset_password/', views.reset_password, name='reset_password'),
-
+    path('vendor/', include('vendor.urls')),
 ]
